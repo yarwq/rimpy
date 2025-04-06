a38db8c7ec44160a76210596891a63e7991ea178
yarwq <yarwq05@gmail.com>
2025-04-06 19:00:55 -0200

    start project

---
+++ src/main/java/com/rimp/rimpy/RimpyApplication.java
@@ -0,0 +1,13 @@
+package com.rimp.rimpy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RimpyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RimpyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rimp/rimpy/RimpyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rimp.rimpy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RimpyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
